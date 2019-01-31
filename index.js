86058c1652588106809ee3803419742c78e58f7f
François Leurent <131.js@cloudyks.org>
2019-02-01 00:47:05 +0100

    With version parser & pack2guid

--- index.js
+++ index.js
@@ -23,6 +23,21 @@ class msi {
     return guid;
   }
 
+  static Packed2Guid(pack) {
+    pack = msi.Guid2Packed(pack);
+    return `${pack.substr(0, 8)}-${pack.substr(8, 4)}-${pack.substr(12, 4)}-${pack.substr(16, 4)}-${pack.substr(20, 12)}`;
+  }
+
+  static ParseVersion(input) {
+    let version = `0000000${input}`.substr(-8);
+    let major = parseInt(version.substr(0, 2), 16);
+    let minor = parseInt(version.substr(2, 2), 16);
+    let build = parseInt(version.substr(4, 4), 16);
+    return `${major}.${minor}.${build}`;
+  }
+
+
+
 }
 
 
--- test/index.js
+++ test/index.js
@@ -1,18 +1,27 @@
 "use strict";
 
 const Guid2Packed = require('../').Guid2Packed;
+const ParseVersion = require('../').ParseVersion;
 
 const expect = require('expect.js');
 
-describe("Minimal test suite", function(){
+describe("Minimal test suite", function() {
 
-  var port, server;
-
-  it("check Guid2Packed", function(){
-    
+  it("check Guid2Packed", function() {
     var src = "{12345678-ABCD-WXYZ-1234-ABCDEFGHIJKL}";
     expect(Guid2Packed(src)).to.eql("87654321DCBAZYXW2143BADCFEHGJILK");
+  });
 
+
+  it("check ParseVersion", function() {
+    expect(ParseVersion("80f0000")).to.eql("8.15.0");
+    expect(ParseVersion("1a0000")).to.eql("0.26.0");
+    expect(ParseVersion("2073b2e")).to.eql("2.7.15150");
+    expect(ParseVersion("a0142ee")).to.eql("10.1.17134");
+    expect(ParseVersion("e0063bb")).to.eql("14.0.25531");
+    expect(ParseVersion("8646580")).to.eql("8.100.25984");
+    expect(ParseVersion("9050000")).to.eql("9.5.0");
+    expect(ParseVersion("4")).to.eql("0.0.4");
 
   });
 
@@ -20,5 +29,5 @@ describe("Minimal test suite", function(){
 
 
 
-
 });
+
